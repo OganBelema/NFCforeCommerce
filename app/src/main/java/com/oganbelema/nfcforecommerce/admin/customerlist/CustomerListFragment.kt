7f26f81144cb514b716dcc73f1c0ab557181f30d
Belema Ogan <Oganbelema@gmail.com>
2019-05-01 17:56:18 +0100

    Finish up Customer list UI
    - Add required dependencies for lottie animation, recyclerview and floating action button because they will be used in the view
    - Add drawable for the floating action button
    - Add the search animation json to the raw resource directory
    - Change framelayout to constraintlayout to better execute the structuring of the view
    - Add the navigation to the add customer fragment when the add customer button is clicked in the customer list fragment

--- app/src/main/java/com/oganbelema/nfcforecommerce/admin/customerlist/CustomerListFragment.kt
+++ app/src/main/java/com/oganbelema/nfcforecommerce/admin/customerlist/CustomerListFragment.kt
@@ -6,14 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 import com.oganbelema.nfcforecommerce.R
+import kotlinx.android.synthetic.main.customer_list_fragment.*
 
 class CustomerListFragment : Fragment() {
 
     companion object {
         fun newInstance() = CustomerListFragment()
     }
+
+    private lateinit var navController: NavController
 
     private lateinit var viewModel: CustomerListViewModel
 
@@ -22,6 +27,16 @@ class CustomerListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.customer_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = view.findNavController()
+
+        addCustomerButton.setOnClickListener {
+            navController.navigate(R.id.action_customerListFragment_to_addCustomerFragment)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
