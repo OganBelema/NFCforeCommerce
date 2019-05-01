a83bc280d306e5fab7211c69528ae26fc712252b
Belema Ogan <Oganbelema@gmail.com>
2019-05-01 14:52:21 +0100

    Enable Activity to Receive NFC event
    - Refactor the checkNfcAvailability method to make it reusable to check
    for nfc availability
    - Add method to enable activity to be dispatched when nfc tag is
    discovered and activity is in foreground
    - Add method to disable activity from being dispatched when it is not
    in foreground

--- app/src/main/java/com/oganbelema/nfcforecommerce/base/BaseActivity.kt
+++ app/src/main/java/com/oganbelema/nfcforecommerce/base/BaseActivity.kt
@@ -1,5 +1,8 @@
 package com.oganbelema.nfcforecommerce.base
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
 import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -24,15 +27,7 @@ class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
 
-        checkNfcAvailability()
-    }
-
-    /**
-     * Checks that the device has nfc capability and that nfc is enabled
-     * Displays a toast message for each case
-     */
-    private fun checkNfcAvailability() {
-        if (nfcAdapter != null && nfcAdapter!!.isEnabled) {
+        if (isNfcAvailable()){
             Toast.makeText(this, "Nfc ia available", Toast.LENGTH_LONG)
                 .show()
         } else {
@@ -40,4 +35,51 @@ class BaseActivity : AppCompatActivity() {
                 .show()
         }
     }
+
+    /**
+     * Checks that the device has nfc capability and that nfc is enabled
+     * @return true if nfc feature is available and enabled
+     * @return false if it's not
+     */
+    private fun isNfcAvailable(): Boolean {
+        return nfcAdapter?.isEnabled ?: false
+    }
+
+    /**
+     * This method enables the activity to be called when an nfc tag is discovered
+     * It should be only called when activity is in foreground (resumed) state
+     * @throws IllegalStateException if the Activity is not currently in the foreground
+     * @throws UnsupportedOperationException if FEATURE_NFC is unavailable.
+     */
+    private fun enableForegroundDispatch(){
+        if (isNfcAvailable()){
+            val nfcIntent = Intent(this, BaseActivity::class.java)
+            nfcIntent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING)
+            val pendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0)
+            val intentFilter = Array(0){ IntentFilter() }
+            nfcAdapter?.enableForegroundDispatch(this, pendingIntent, intentFilter, null)
+        }
+    }
+
+    /**
+     * This method disables the activity form being called when an nfc tag is discovered
+     * this is for when activity is not in foreground state
+     */
+    private fun disableForegroundDispatch() {
+        if (isNfcAvailable()){
+            nfcAdapter?.disableForegroundDispatch(this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        enableForegroundDispatch()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disableForegroundDispatch()
+    }
+
+
 }
